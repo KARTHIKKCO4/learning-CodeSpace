b2f424019de5a7dc4e9eb20ec0265fe7faf30263
karthik <100414831+KARTHIKKCO4@users.noreply.github.com>
2024-02-05 14:31:54 +0530

    Update About.jsx
    updating about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a AI/ML student studying at Dayananda Sagar College Of Engineering , Bangalore. I enjoy creating unique and simplistic web developments and Machine Learning Projects.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Machine Learning",
+  "MySQL",
+  "Data Structures and Algorithms",
+  "Javascript",
+  "Java and Python",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my web development experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
